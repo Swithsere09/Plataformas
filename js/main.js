f1e15e2bd02f964c8aa73562bf3d5159fd2f3094
Daniel Castillo <daniel.castillob@upb.edu.co>
2020-07-29 22:30:18 -0500

    Actualizacion index - Departamentos

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const expresiones = {
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	contrasena: /^.{4,12}$/ // 4 a 12 digitos.
+    contrasena: /^.{4,12}$/ // 4 a 12 digitos.
 }
 
 const campos = {
@@ -17,41 +17,41 @@ const campos = {
     contrasena: false
 }
 
-const validarFormulario = (e)=>{
+const validarFormulario = (e) => {
     // console.log(e.target.name);
-    switch(e.target.name){
+    switch (e.target.name) {
         case "usuario":
             validarCampo(expresiones.usuario, e.target, e.target.name);
-        break;
+            break;
         case "correo":
             validarCampo(expresiones.correo, e.target, e.target.name);
-        break;
+            break;
         case "nombres":
             validarCampo(expresiones.nombre, e.target, e.target.name);
-        break;
+            break;
         case "apellidos":
             validarCampo(expresiones.apellido, e.target, e.target.name);
-        break;
+            break;
         case "Contrasena":
             validarCampo(expresiones.contrasena, e.target, e.target.name);
             ValidarContrasena2();
-        break;
+            break;
         case "Contrasena2":
             ValidarContrasena2();
-        break;
+            break;
     }
-    
+
 }
 
-const validarCampo = (expresion,input,campo) => {
-    if(expresion.test(input.value)){
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
         document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
         document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
         campos[campo] = true;
-    }else{
+    } else {
         document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
         document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
@@ -65,14 +65,14 @@ const ValidarContrasena2 = () => {
     const inputPassword1 = document.getElementById('Contrasena');
     const inputPassword2 = document.getElementById('Contrasena2');
 
-    if(inputPassword1.value !== inputPassword2.value){
+    if (inputPassword1.value !== inputPassword2.value) {
         document.getElementById(`grupo__contrasena2`).classList.remove('formulario__grupo-correcto');
         document.getElementById(`grupo__contrasena2`).classList.add('formulario__grupo-incorrecto');
         document.querySelector(`#grupo__contrasena2 i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo__contrasena2 i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo__contrasena2 .formulario__input-error`).classList.add('formulario__input-error-activo');
         campos['contrasena'] = false;
-    }else{
+    } else {
         document.getElementById(`grupo__contrasena2`).classList.remove('formulario__grupo-incorrecto');
         document.getElementById(`grupo__contrasena2`).classList.add('formulario__grupo-correcto');
         document.querySelector(`#grupo__contrasena2 i`).classList.remove('fa-times-circle');
@@ -83,27 +83,27 @@ const ValidarContrasena2 = () => {
 }
 
 inputs.forEach((input) => {
-    input.addEventListener('keyup',validarFormulario);
-    input.addEventListener('blur',validarFormulario);
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
 })
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(campos.usuario && campos.correo && campos.nombres && campos.apellidos && campos.contrasena){
+    if (campos.usuario && campos.correo && campos.nombres && campos.apellidos && campos.contrasena) {
         formulario.reset();
 
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
         document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
 
-        setTimeout(()=>{
+        setTimeout(() => {
             document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 
-            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono)=>{
+            document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
                 icono.classList.remove('formulario__grupo-correcto');
             });
-        },3000);
-    }else{
+        }, 3000);
+    } else {
         document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
     }
-})
+});
